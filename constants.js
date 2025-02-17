c7c657b75fb5fccfa28e800a194c7fd5f33396c8
LexxXell <lexxxell007@gmail.com>
2025-02-17 20:45:29 +0400

    Added getDocumentById util and code review for utils

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+export const APP_NAME = 'TrustlessPooledStaking';
+
 export const BASE_DIR_NAME = ".dashtrustlessstaking"
 export const CONFIG_FILENAME = "config.json"
 export const CACHE_FILENAME = "sdkWalletCache.json"
