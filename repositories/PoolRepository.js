2c729b68b52d2aeb2218fd21b10e1a0ed5a2c7f7
LexxXell <lexxxell007@gmail.com>
2025-02-20 19:14:25 +0400

    Added create pool to PoolRepository

--- repositories/PoolRepository.js
+++ repositories/PoolRepository.js
@@ -1,6 +1,11 @@
 import Dash from "dash";
 import {APP_NAME} from "../constants.js";
 import Pool from "../models/Pool.js";
+import PoolStatusEnum from "../models/enums/PoolStatusEnum.js";
+import MasternodeTypeEnum from "../models/enums/MasternodeTypeEnum.js";
+import InvalidPoolTypeError from "../errors/InvalidPoolTypeError.js";
+import logger from "../logger.js";
+import config from "../config.js";
 
 const Client = Dash.Client;
 
@@ -31,6 +36,36 @@ class PoolRepository {
     if (!poolDocument) {
       return null;
     }
+
+    return Pool.fromDocument(poolDocument);
+  }
+
+  /**
+   * @param {Pool} pool - Thr Pool instance to create
+   * @returns {Promise<Pool>}
+   */
+  async create(pool) {
+    const { platform } = this.sdk;
+
+    logger.info(`Creating pool: ${pool.name}`);
+
+    const identity = await platform.identities.get(config.identity);
+
+    const poolDocument = await platform.documents.create(
+      `${APP_NAME}.${this.#docName}`,
+      identity,
+      {...pool, createdAt: undefined, updatedAt: undefined},
+    )
+
+    const documentBatch = {
+      create: [poolDocument],
+      replace: [],
+      delete: [],
+    };
+
+    logger.log("Broadcasting Pool Document");
+    await platform.documents.broadcast(documentBatch, identity);
+    logger.log("Done..",`Pool Document at: ${poolDocument.getId()}`);
 
     return Pool.fromDocument(poolDocument);
   }
