5b20914d6a01eb1d9ae94a0f88567ea8935b22b1
LexxXell <lexxxell007@gmail.com>
2025-08-02 14:09:19 +0400

    Added signActionProposalAction

--- actions/signActionProposalAction.js
+++ actions/signActionProposalAction.js
@@ -1,8 +1,59 @@
+import bs58 from "bs58";
+import dashcore from "@dashevo/dashcore-lib";
 import logger from "../logger.js";
+import config from "../config.js";
+import ActionProposalRepository from "../repositories/ActionProposalRepository.js";
+import CollateralRepository from "../repositories/CollateralRepository.js";
+import PoolRepository from "../repositories/PoolRepository.js";
+import PoolNotFoundError from "../errors/PoolNotFoundError.js";
+import UtxoNotFoundError from "../errors/UtxoNotFoundError.js";
+import UserNotInPoolError from "../errors/UserNotInPoolError.js";
+import ActionProposalNotFoundError from "../errors/ActionProposalNotFoundError.js";
+
+const {PrivateKey, Address, Script, Transaction} = dashcore;
 
 const signActionProposalAction = (sdk) => {
   return async (proposalId) => {
+    const actionProposalSignatureRepository = new ActionProposalRepository(sdk);
+    const actionProposalRepository = new ActionProposalRepository(sdk);
+    const poolRepository = new PoolRepository(sdk);
+    const collateralRepository = new CollateralRepository(sdk);
+
+    const actionProposal = await actionProposalRepository.getById(proposalId);
+    if (!actionProposal) {
+      throw new ActionProposalNotFoundError();
+    }
+
+    const pool = await poolRepository.getById(actionProposal.poolId);
+    if (!pool) {
+      throw new PoolNotFoundError(actionProposal.poolId)
+    }
+
+    const collateralUTXOs = await collateralRepository.getByPoolId(bs58.decode(actionProposal.poolId));
+    if (!collateralUTXOs?.length) {
+      throw new UtxoNotFoundError();
+    }
+
+    const identity = await sdk.identities.getIdentityByIdentifier(config.identity);
+    const isMember = collateralUTXOs.some((doc) => doc.ownerId === identity.id.base58());
+    if (!isMember) {
+      throw new UserNotInPoolError(actionProposal.poolId);
+    }
+
     logger.info(`Signing ActionProposal: ${proposalId}`);
+
+    const tx = new Transaction(actionProposal.transactionHex)
+
+    const wallet = await sdk.keyPair.mnemonicToWallet(config.mnemonic);
+
+    const privateKey = new PrivateKey(
+      "e8d594cb3cd2df1b23237ed771d968b7d30bb436b8ac1bc45b8496512984924e",
+      config.network
+    );
+
+    const [signature] = tx.getSignatures(privateKey)
+
+    logger.info(signature.toString());
   };
 };
 
